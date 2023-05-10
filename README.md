18ae3cc1f11e45f4eee912a9475b52c4714cf0cf
EbonyRFord <126352317+EbonyRFord@users.noreply.github.com>
2023-05-10 00:06:05 -0400

    final

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
-# Professional README Generator Starter Code
+# Quality README Generator
+ 
+  # Description of Project
+  The quality README Generator will make it so users can generate a descriptive readme without much thought by utilizing the inquirer package and Nodejs.
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+  #Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Resouces](#resources)
+* [Tests](#tests)
+* [Questions](#questions)
+
+
+ #Installation Guide
+  The user will need inquirer, and need to run npm install in the command line of their terminal, from there they can run node index.js.
+
+  #Usage
+  This project will save developers time and provide descriptions that give the end user of the application an easy way to navigate as well.
+
+  #Liscense
+  undefined
+
+  #Resources
+  I used Youtube as a resource.
+
+  #Tests (Optional)
+  N/A
+
+  ## If you have any questions regarding this project please contact: 
+  marketingwithebony@gmail.com
+
+Video Link: https://drive.google.com/file/d/1gxcAU4gj6kRH4fHrCsW8Vb33GyDBf0YT/view
+
--- index.js
+++ index.js
@@ -1,13 +1,82 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require('fs');
+const utils = require("util");
+const path = require('path')
+const generateMarkdown = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: "Title",
+        type: "input",
+        message: "List the project's title"
+    },
+    {
+        name: "Description",
+        type: "input",
+        message: "Describe the project"
+    },
+    {
+        name: "Installation",
+        type: "input",
+        message: "How does the user install this project? Are any dependencies required?"
+    },
+    {
+        name: "Usage",
+        type: "input",
+        message: "What was your motivation for the project and what problem's will the project solve?"
+    },
+
+    {
+        name: "Contributing",
+        type: "input",
+        message: "What resources did you use?"
+    },
+    {
+        name: "Tests",
+        type: "input",
+        message: "Are there any Tests?"
+    },
+    {
+        name: "Questions",
+        type: "email",
+        message: "What is your email address?"
+    },
+    {
+        name: "GitHub",
+        type: "input",
+        message: "What is your Github username?"
+    },
+    {
+        name: "License",
+        type: "input",
+        message: "What License was used?"
+    },
+];
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err) {
+        console.log(fileName)
+        console.log(data)
+        if (err) {
+            return console.log("You missed a step")
+        }
+
+    })
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(function (data) {
+        writeToFile("README.md", generateMarkdown(data));
+    });
+}
 
 // Function call to initialize app
 init();
